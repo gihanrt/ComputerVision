d73232d684a4bd03d1ca4bb51706c752c888bdc5
gihantharanga <tharangarpg@gmail.com>
2015-01-01 00:14:52 -0800

    cv init

--- projectcV/main.cpp
+++ projectcV/main.cpp
@@ -3,12 +3,19 @@
 #include <iostream>
 #include <string>
 
+#include "opencv2\core\core.hpp"
+#include "opencv2\contrib\contrib.hpp"
+#include "opencv2\highgui\highgui.hpp"
+#include "opencv2\objdetect\objdetect.hpp"
+#include "opencv2\opencv.hpp"
+
 using namespace std;
+using namespace cv;
 
 int main()
 {
 	
-	cout << " hello world " << endl;
+	 
 
 	system("pause");
 	return 0;
