4577e366e96df61d77090719fc03fe847c884400
gihantharanga <tharangarpg@gmail.com>
2015-01-01 00:05:37 -0800

    init

---
+++ projectcV/main.cpp
@@ -0,0 +1,15 @@
+/*author gihan tharanga*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << " hello world " << endl;
+
+	system("pause");
+	return 0;
+}
