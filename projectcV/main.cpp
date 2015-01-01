45017935d059a9043b6d367fa348a0e131d2649b
gihantharanga <tharangarpg@gmail.com>
2015-01-01 00:24:02 -0800

    reading image

--- projectcV/main.cpp
+++ projectcV/main.cpp
@@ -16,7 +16,17 @@ int main()
 {
 	
 	 
+	Mat image = imread("C://Users/lsf-admin/Desktop/UN/boganbara/DSC_0508.jpg",CV_LOAD_IMAGE_COLOR);
+	if (!image.data){
+		cout << "coudnot open" << endl;
+		return -1;
+	}
 
+	namedWindow("window", WINDOW_NORMAL);
+	imshow("window", image);
+	
+	waitKey(10000);
+	destroyWindow("window");
 	system("pause");
 	return 0;
 }
